f5de3dcc0bda516c9bbe3b9ba7af69b65391cb2c
FranjoHHZ <167630679+FranjoHHZ@users.noreply.github.com>
2025-01-07 20:20:47 +0100

    Update test_runtime.py

--- test_runtime.py
+++ test_runtime.py
@@ -1,25 +1,35 @@
+import sys
+import time
 import unittest
 import numpy as np
 from main_runtime import train_model, download_and_prepare_data
+
+# Alle Ausgaben in die Datei "ausgabe2.txt" umleiten
+sys.stdout = open("ausgabe2.txt", "w")
 
 class TestModel(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        print("Setting up class for runtime tests.\n")
         cls.X, cls.y = download_and_prepare_data()
-        cls.model = train_model(cls.X[:60000], cls.y[:60000])
     
     def test_predict_accuracy(self):
-        y_pred = self.model.predict(self.X[60000:])
+        model = train_model(self.X[:60000], self.y[:60000])
+        y_pred = model.predict(self.X[60000:])
         accuracy = np.mean(y_pred == self.y[60000:])
+        print(f"Test Accuracy: {accuracy * 100:.2f}%\n")
         self.assertGreater(accuracy, 0.70, "Accuracy should be greater than 70%")
-    
+
     def test_training_time(self):
-        import time
         start_time = time.time()
         train_model(self.X[:60000], self.y[:60000])
         end_time = time.time()
-        self.assertLess(end_time - start_time, 300, "Training time should be less than 300 seconds")
+        print(f"Training time: {end_time - start_time:.5f} sec\n")
+        self.assertLess(end_time - start_time, 274, "Training time should be less than 274 seconds")
 
 if __name__ == "__main__":
     unittest.main()
+
+# Datei schließen
+sys.stdout.close()
